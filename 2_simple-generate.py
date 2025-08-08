d21cd7d24ed88c6836530b81e869a61e488552fc
teklu <teklufa@gmail.com>
2025-08-08 14:10:19 +0300

    Add dotenv for environment incase we want to change the model we wanted to use.

--- 2_simple-generate.py
+++ 2_simple-generate.py
@@ -1,7 +1,11 @@
 from langchain_ollama import ChatOllama
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
 
 # LangChain automatically communicates with your running Ollama instance.
 llm = ChatOllama(model="llama3")
 
 result = llm.invoke("who are you? What is 81 divided by 9?, and what is the capital of France? can i ask you any question?")
 print(result.content)
+print(result)
