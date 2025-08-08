73136f36d56dca6e00e923871b63dc41d0fddc9f
teklu <teklufa@gmail.com>
2025-08-08 16:42:17 +0300

    prompt template with message

--- 2_prompt-templates/1_basic.py
+++ 2_prompt-templates/1_basic.py
@@ -1,7 +1,12 @@
 from langchain.prompts import ChatPromptTemplate
 
-template = "Tell me {number} jokes about {topic}."
-prompt_template = ChatPromptTemplate.from_template(template)
+# template = "Tell me {number} jokes about {topic}."
+message = [
+  ("system", "You are a comedian who tells jokes about {topic}."),
+  ("human", "Tell me {count} jokes")
+]
+# prompt_template = ChatPromptTemplate.from_template(template)
+prompt_template = ChatPromptTemplate.from_messages(message)
 
-prompt = prompt_template.invoke({"topic": "cats", "number": 3})
+prompt = prompt_template.invoke({"topic": "lawyers", "count": 3})
 print(prompt)
