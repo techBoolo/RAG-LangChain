22f49f7d1fd6981ef111fbefbb1148d7ae63b4e5
teklu <teklufa@gmail.com>
2025-08-09 12:41:35 +0300

    branching

---
+++ 3_chain/5_branching.py
@@ -0,0 +1,77 @@
+from langchain_ollama import ChatOllama
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage
+from langchain_core.runnables import RunnableLambda, RunnableParallel, RunnableBranch
+from langchain_core.output_parsers import StrOutputParser
+
+llm = ChatOllama(model="llama3")
+
+message = [
+  ("system", "You are a helpful assistant."),
+  # HumanMessage(content="Tell me 3 joks").   # if we use variable we have to use tuple instead of HumanMessage
+  ("user", "List the main features of the product {product_name}.")
+]
+prompt_template = ChatPromptTemplate.from_messages(message)
+
+# positive feedback
+postive_feedback_message =  [
+  ("system", "You are a helpful assistant."),
+  ("human", "")
+]
+positive_feedback_template = ChatPromptTemplate.from_messages(postive_feedback_message)
+
+# negative feedback
+negative_feedback_message =   [
+    ("system", "You are a helpful assistant."),
+    ("human", "")
+  ]
+negative_feedback_template = ChatPromptTemplate.from_messages(negative_feedback_message)
+
+# neutral feedback
+neutral_feedback_message = [
+  ("system", "You are a helpful assistant."),
+  ("user", "Generate a request for more details for this neutral feedback {feedback}")
+]
+neutral_feedback_template = ChatPromptTemplate.from_messages(neutral_feedback_message)
+
+# escaltle feedback
+escalate_feedback_message = [
+  ("system", "You are a helpful assistant"),
+  ("human", "Generate a message to escalate this feedback to a human agent {feedback}.")
+]
+escalate_feedback_template = ChatPromptTemplate.from_messages(escalate_feedback_message)
+
+branches = RunnableBranch(
+  (
+    lambda x: "positive" in x,
+    positive_feedback_template | llm | StrOutputParser()
+  ),
+  (
+    lambda x: "negative" in x,
+    negative_feedback_template | llm | StrOutputParser()
+  ),
+  (
+    lambda x: "neutral" in x,
+    neutral_feedback_template | llm | StrOutputParser()
+  ),
+  escalate_feedback_template | llm | StrOutputParser()
+) 
+
+classification_template = ChatPromptTemplate.from_messages(
+  [
+    ("system", "You are a helpful assistant."),
+    ("user", "Classify the sentiment of this feedback as positive, negative, neutral or escalette {feedback}")
+  ]
+)
+
+classification_chain = classification_template | llm | StrOutputParser()
+chain = classification_chain | branches
+
+positive_review = f"The product is excellent, I really enjoy using it and found it very helpful."
+negative_review = f"The product is terrible, It broke just after one use and the quality is very poor."
+neutral_review = f"The product is Okay, It works as expected but nothing exceptional."
+escalate_review = f"I am not sure about the product yet, can you tell me more about its feature."
+
+result = chain.invoke({"feedback": negative_review})
+print(result)
+
